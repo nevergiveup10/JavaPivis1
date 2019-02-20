d6f7395c70212bd18c5f14990bdcada634bbf4a6
nevergiveup10 <smolensky.p@gmail.com>
2019-02-20 19:46:35 +0300

    1st widget added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,54 @@
 package application;
-	
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
+//public class FirstWidget extends Application {
+//}
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			Label label = new Label ("Виджет №1:");
+			Button button1 = new Button("Внести в список");           // кнопка
+			TextField tfield1 = new TextField ("Введите текст");
+			ComboBox combobox1 = new ComboBox();
+			button1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+	        public void handle(ActionEvent event) {
+			combobox1.getItems().addAll(tfield1.getText());
+			combobox1.setValue(tfield1.getText());
+	          button1.setText("You've clicked!");
+			}
+			});
+			
+			
+	        FlowPane root = new FlowPane(10, 10, label, tfield1, button1, combobox1); // FlowPane располагает элементы в строку друг за другом
+	        root.setOrientation(Orientation.VERTICAL);
+	  //      Pane pane1 = new Pane();
+			Scene scene = new Scene(root,2000,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Lab 1, Smolensky P.");
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
 	}
+	
+
 	
 	public static void main(String[] args) {
 		launch(args);
