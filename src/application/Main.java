2d53690d6d78a1d715175fa31ffe25a5c75e9d93
nevergiveup10 <smolensky.p@gmail.com>
2019-02-26 11:07:25 +0300

    4th widget added

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
@@ -66,6 +67,18 @@ public class Main extends Application {
 	        radButList.add(radBut3);
 			Separator sep3 = new Separator();
 			
+			Label label4 = new Label ("Виджет №4:");
+			TextField tfield4 = new TextField ("Введите имя объекта");
+			Button button5 = new Button("Выбрать объект");
+			CheckBox chkBox1 = new CheckBox("1");
+			CheckBox chkBox2 = new CheckBox("2");
+			CheckBox chkBox3 = new CheckBox("3");
+	        ArrayList<CheckBox> chkBoxList = new ArrayList<CheckBox>();
+	        chkBoxList.add(chkBox1);
+	        chkBoxList.add(chkBox2);
+	        chkBoxList.add(chkBox3);
+			Separator sep4 = new Separator();
+			
 			Alert alert2 = new Alert(AlertType.WARNING);
 			alert2.setTitle("Ошибка!");
 			alert2.setHeaderText("Такого элемента не существует.");
@@ -103,24 +116,44 @@ public class Main extends Application {
 			button4.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 		        public void handle(ActionEvent event) {
-					RadioButton select =  radButList.stream()
+					RadioButton select1 =  radButList.stream()
 		                    .filter(radioButton -> radioButton.getText().equals(tfield3.getText()))
 		                    .findAny()
 		                    .orElse(null);
-		            if(select!= null){
-		            	radButgroup.selectToggle(select);
+		            if(select1!= null){
+		            	radButgroup.selectToggle(select1);
 		            } else {
 		            	alert2.showAndWait();
 		            }
 			
 				}
 				});
+			
+			button5.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+		        public void handle(ActionEvent event) {
+					CheckBox select2 =  chkBoxList.stream()
+		                    .filter(checkBox -> checkBox.getText().equals(tfield4.getText()))
+		                    .findAny()
+		                    .orElse(null);
+		            if(select2!= null){
+		            	if(select2.isSelected()==false) {
+		            		select2.fire();
+		            		}
+		            }	
+		             else {
+		            	alert2.showAndWait();
+		            }
+			
+				}
+				}); 
 				
 			VBox pane1 = new VBox (10, label, tfield1, button1, combobox1, sep1);
 			VBox pane2 = new VBox(10, label2, tfield2, button2, button3, sep2);
-			VBox pane3 = new VBox (10, label3, tfield3, button4, radBut1, radBut2, radBut3);
-			VBox root = new VBox(10, pane1, pane2, pane3);
-			Scene scene = new Scene(root,300,800);
+			VBox pane3 = new VBox (10, label3, tfield3, button4, radBut1, radBut2, radBut3, sep3);
+			VBox pane4 = new VBox (10, label4, tfield4, button5, chkBox1, chkBox2, chkBox3, sep4);
+			VBox root = new VBox(10, pane1, pane2, pane3, pane4);
+			Scene scene = new Scene(root,300,1000);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Lab 1, Smolensky P.");
